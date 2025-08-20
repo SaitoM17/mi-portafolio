eac7207659b3a611b5f09ed63e058ee1ea84e47e
Said Mariano <smariano170@gmail.com>
2025-08-20 13:20:02 -0600

    feat: Actualización a los proyectos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,16 +3,28 @@ import React from "react";
 function Projects() {
   const proyectos = [
     {
-      nombre: "📊 Análisis de Clima Mundial",
-      descripcion: "Limpieza y análisis de datos del NOAA Weather Data.",
+      nombre: "📊 SuperStore Sales",
+      descripcion: "Este proyecto consiste en un dashboard interactivo y la generación de reportes ejecutivos. Ambas herramientas están diseñadas para analizar el rendimiento de la entrega de pedidos. Los datos utilizados provienen de un conjunto de datos real de entregas recopilado por Rohit Sahoo.",
       herramientas: "Python, Pandas, SQL",
-      link: "https://github.com/tuusuario/proyecto-clima",
+      link: "https://github.com/SaitoM17/superstore_sales",
     },
     {
-      nombre: "📈 Dashboard de Ventas",
-      descripcion: "Dashboard interactivo para KPIs de ventas.",
+      nombre: "📊 Ramen Ratings",
+      descripcion: "Este proyecto presenta un dashboard interactivo basado en más de 2500 reseñas de productos de ramen a nivel internacional, recopiladas por Aleksey Bilogur (Owner).",
       herramientas: "Power BI",
-      link: "https://app.powerbi.com/view?r=tu_dashboard",
+      link: "https://github.com/SaitoM17/ramen_ratings",
+    },
+    {
+      nombre: "📊 Amazon Delivery",
+      descripcion: "Este proyecto consiste en un dashboard interactivo y la generación de reportes ejecutivos. Ambas herramientas están diseñadas para analizar el rendimiento de la entrega de pedidos. Los datos utilizados provienen de un conjunto de datos real de entregas recopilado por Sujal Suthar, con información valiosa para entender y mejorar la eficiencia operativa.",
+      herramientas: "Python, Pandas, SQL",
+      link: "https://github.com/SaitoM17/amazon_delivery",
+    },
+    {
+      nombre: "📊 Precio de las tortillas en México",
+      descripcion: "Este proyecto realiza un análisis exploratorio y descriptivo del precio de la tortilla en México. El conjunto de datos incluye información detallada sobre el precio por kilogramo de la tortilla en diversas ubicaciones (estados y ciudades) y tipos de tiendas a lo largo del tiempo, abarcando el periodo 2007-2024.",
+      herramientas: "Power BI",
+      link: "https://github.com/SaitoM17/analisis_del_precio_tortillas_mexico_2007_a_2024",
     },
   ];
 
