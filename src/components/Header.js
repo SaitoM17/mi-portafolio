612ca9c797dc35a5575ce38b0b66991c08b71e42
Said Mariano <smariano170@gmail.com>
2025-08-20 10:46:26 -0600

    feat: Creación de header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Saito</h1>
+      <p>Analista de Datos Jr | SQL • Python • Power BI</p>
+    </header>
+  );
+}
+
+export default Header;
