8f4e9b87d99d7ddf8aa65e8feafafc2afe6c73c0
Said Mariano <smariano170@gmail.com>
2025-08-21 14:05:04 -0600

    feat: Se actualizo el App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
+// import React from "react";
+// import Header from "./components/Header";
+// import About from "./components/About";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <About />
+//       <Projects />
+//       <Contact />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
@@ -6,15 +29,27 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import "./App.css";
 
+// Importa tus páginas de proyecto
+import SuperStoreSales from "./pages/SuperStore_Sales.js";
+// import RamenRatings from "./pages/Ramen_Ratings";
+
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <About />
-      <Projects />
-      <Contact />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <About />
+            <Projects />
+            <Contact />
+          </>
+        }/>
+        <Route path="/superstore-sales" element={<SuperStoreSales />} />
+        {/* <Route path="/ramen-ratings" element={<RamenRatings />} /> */}
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
