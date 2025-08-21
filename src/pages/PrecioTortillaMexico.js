c2287378c3bacc287041e3da905bece2fb3b1c7c
Said Mariano <smariano170@gmail.com>
2025-08-21 17:06:39 -0600

    feat: Construcción beta de la pagina del proyecto

---
+++ src/pages/PrecioTortillaMexico.js
@@ -0,0 +1,21 @@
+function PrecioTortilla() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>📊 Precio de las tortillas en México</h2>
+      <p>
+        Este proyecto realiza un análisis exploratorio y descriptivo del precio 
+        de la tortilla en México. El conjunto de datos incluye información detallada 
+        sobre el precio por kilogramo de la tortilla en diversas 
+        ubicaciones (estados y ciudades) y tipos de tiendas a lo largo del tiempo, abarcando 
+        el periodo 2007-2024.
+      </p>
+      <img src="/img/Tortillas.png" alt="Tortilla" style={{ maxWidth: "400px" }} />
+      <br />
+      <a href="https://github.com/SaitoM17/analisis_del_precio_tortillas_mexico_2007_a_2024.git" target="_blank" rel="noopener noreferrer">
+        Ver en GitHub
+      </a>
+    </div>
+  );
+}
+
+export default PrecioTortilla;
