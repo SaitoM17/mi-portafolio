9baf3f40bcbcc31ed7a23fc5a7383fe872776549
Said Mariano <smariano170@gmail.com>
2025-08-21 14:06:08 -0600

    feat: Se creo carpeta con paginas de cada proyecto

---
+++ src/pages/SuperStore_Sales.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function SuperStoreSales() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>📦 SuperStore Sales</h2>
+      <p>
+        Este proyecto consiste en un dashboard interactivo y reportes
+        ejecutivos para analizar la entrega de pedidos.  
+        Incluye análisis en Python, Pandas y SQL.
+      </p>
+      <img src="/img/SuperStore_Sales.jpg" alt="Amazon Delivery" style={{ maxWidth: "400px" }} />
+      <br />
+      <a href="https://github.com/SaitoM17/amazon_delivery" target="_blank" rel="noopener noreferrer">
+        Ver en GitHub
+      </a>
+    </div>
+  );
+}
+
+export default SuperStoreSales;
