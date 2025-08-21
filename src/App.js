c34c761c0a940d349c250b0f6e316e7b1fa858e6
Said Mariano <smariano170@gmail.com>
2025-08-21 16:23:28 -0600

    feat: Se agrego rutas de las paginas de los proyectos

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 import SuperStoreSales from "./pages/SuperStore_Sales.js";
+import RamenRantings from "./pages/RamenRating.js";
+import AmazonDelivery from "./pages/AmazonDelivery.js";
+import PrecioTortilla from "./pages/PrecioTortillaMexico.js";
+import SIEM from "./pages/SIEM.js";
+import MexicoTradeStatis from "./pages/MexicoTradeStati.js";
 
 function App() {
   return (
@@ -22,6 +27,11 @@ function App() {
           </>
         }/>
         <Route path="/superstore-sales" element={<SuperStoreSales />} />
+        <Route path="/Ramen-ratings" element={<RamenRantings />} />
+        <Route path="/Amazon-delivery" element={<AmazonDelivery />} />
+        <Route path="/Precio-tortilla" element={<PrecioTortilla />} />
+        <Route path="/Siem" element={<SIEM />} />
+        <Route path="/Mexico-trade" element={<MexicoTradeStatis />} />
       </Routes>
       <Footer />
     </Router>
