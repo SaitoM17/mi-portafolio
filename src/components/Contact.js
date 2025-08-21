8400879708106029dc885dffb0d1df243e5f94e2
Said Mariano <smariano170@gmail.com>
2025-08-21 11:07:13 -0600

    feat: Actualización de cantactos

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,12 +6,24 @@ function Contact() {
       <h2>Contacto</h2>
       <p>📧 saito@email.com</p>
       <p>
-        <a href="https://www.linkedin.com/in/tuusuario" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/said-mariano/" target="_blank" rel="noopener noreferrer">
           LinkedIn
         </a>{" "}
         |{" "}
-        <a href="https://github.com/tuusuario" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/saitoM17" target="_blank" rel="noopener noreferrer">
           GitHub
+        </a>{" "}
+        |{" "}
+        <a href="https://public.tableau.com/app/profile/said.mariano/vizzes" target="_blank" rel="noopener noreferrer">
+          Tableu Public
+        </a>{" "}
+        |{" "}
+        <a href="https://github.com/tuusuario" target="_blank" rel="noopener noreferrer">
+          Ubicación: México
+        </a>{" "}
+        |{" "}
+        <a href="https://github.com/tuusuario" target="_blank" rel="noopener noreferrer">
+          Disponibilidad para trabajo remoto
         </a>
       </p>
     </section>
