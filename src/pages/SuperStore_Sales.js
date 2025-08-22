d371ca5dc787853a5948976a621e081e689758e3
Said Mariano <smariano170@gmail.com>
2025-08-22 13:05:25 -0600

    feat: Se añadio propósito del proyecto y reubico la imagen

--- src/pages/SuperStore_Sales.js
+++ src/pages/SuperStore_Sales.js
@@ -3,14 +3,41 @@ import React from "react";
 function SuperStoreSales() {
   return (
     <div style={{ padding: "2rem" }}>
+      <img src="/img/SuperStore_Sales.jpg" alt="Amazon Delivery" style={{ maxWidth: "400px" }} />
+      <br />
       <h2>📦 SuperStore Sales</h2>
+      
       <p>
         Este proyecto consiste en un dashboard interactivo y reportes
         ejecutivos para analizar la entrega de pedidos.  
         Incluye análisis en Python, Pandas y SQL.
       </p>
-      <img src="/img/SuperStore_Sales.jpg" alt="Amazon Delivery" style={{ maxWidth: "400px" }} />
-      <br />
+      
+      <p>
+        🎯 Propósito
+        El proyecto busca evaluar el comportamiento de las ventas de una tienda minorista a partir de datos históricos, considerando diferentes dimensiones clave como producto, cliente, categoría y región. El objetivo es descubrir patrones relevantes, identificar áreas de oportunidad, anticipar riesgos potenciales y establecer estrategias basadas en datos que impulsen decisiones comerciales informadas.
+
+        Obtener KPI's:
+
+        KPI's de Ventas y Rendimiento Financiero
+        KPI's de Eficiencia Operativa
+        KPI's de Clientes y Mercado
+        Analizar el rendimiento de ventas
+
+        Analizar los ingresos por productos vendidos
+        Analizar su comportamiento en distintos contextos
+        Análisis de múltiples dimensiones (Productos, clientes, categorías y región)
+
+        Detectar oportunidades de mejora
+
+        ¿Dónde se puede aumentar ventas, eficiencia o márgenes?
+        Identificar riesgos
+
+        ¿Qué productos, categorías, clientes o región tiene bajas las ventas y representas un riesgo?
+        Encontrar palancas estratégicas
+
+        ¿Qué están funcionando bien y si se puede escalar?
+      </p>
       <a href="https://github.com/SaitoM17/amazon_delivery" target="_blank" rel="noopener noreferrer">
         Ver en GitHub
       </a>
