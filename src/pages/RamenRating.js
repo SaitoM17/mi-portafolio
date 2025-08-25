2d9d235989a95084c99e8410a51276539e3c798e
Said Mariano <smariano170@gmail.com>
2025-08-25 16:54:26 -0600

    feat: Añadio contenedor pagina-proyecto y contenedor de proyecto, titulo y imagen

--- src/pages/RamenRating.js
+++ src/pages/RamenRating.js
@@ -1,17 +1,14 @@
+import "./proyectPages.css";
+
 function RamenRantings() {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h2>🍵 Ramen Ratings</h2>
-      <p>
-        Este proyecto presenta un dashboard interactivo basado en más 
-        de 2500 reseñas de productos de ramen a nivel internacional, recopiladas 
-        por Aleksey Bilogur.
-      </p>
-      <img src="/img/Ramen.jpg" alt="Ramen Rating" style={{ maxWidth: "400px" }} />
-      <br />
-      <a href="https://github.com/SaitoM17/ramen_ratings" target="_blank" rel="noopener noreferrer">
-        Ver en GitHub
-      </a>
+    <div className="pagina-proyecto">
+      <div className="contenedor-proyecto">
+
+        <h1>🍵 Ramen Ratings</h1>
+        <img src="/img/Ramen.jpg" alt="Ramen" className="imagen-principal" />
+        <br/>
+      </div>
     </div>
   );
 }
