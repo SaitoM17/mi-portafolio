802a20de5e9d3139f8a860cc230a345d60b35bf7
Said Mariano <smariano170@gmail.com>
2025-08-25 17:07:17 -0600

    feat: Titulo y descripción del proyecto

--- src/pages/RamenRating.js
+++ src/pages/RamenRating.js
@@ -8,6 +8,12 @@ function RamenRantings() {
         <h1>🍵 Ramen Ratings</h1>
         <img src="/img/Ramen.jpg" alt="Ramen" className="imagen-principal" />
         <br/>
+
+        <h2>Analisis de Ramen (Ramen Ratings Dashboard - Power BI)</h2>
+        <p>
+          Este proyecto presenta un dashboard interactivo basado en más de 2500 reseñas de productos de ramen a nivel internacional, recopiladas por Aleksey Bilogur.
+        </p>
+
       </div>
     </div>
   );
