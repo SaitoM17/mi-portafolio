70b97122db391dd3e6b2c5267de563427a51bbe3
Said Mariano <smariano170@gmail.com>
2025-08-27 15:00:17 -0600

    feat: Se agego sub titulo y descripción del proyecto

--- src/pages/PrecioTortillaMexico.js
+++ src/pages/PrecioTortillaMexico.js
@@ -4,6 +4,13 @@ function PrecioTortilla() {
       <div className="contenedor-proyecto">
         <h1>📊 Precio de las tortillas en México</h1>
         <img src="/img/Tortillas.png" alt="Amazon Delivary" className="imagen-principal" />
+
+        <h2>Análisis del Precio de las Tortillas en México 2007 - 2024</h2>
+        <p>
+          Este proyecto realiza un análisis exploratorio y descriptivo del precio de la tortilla en México. El conjunto de datos incluye información 
+          detallada sobre el precio por kilogramo de la tortilla en diversas ubicaciones (estados y ciudades) y tipos de tiendas a lo largo 
+          del tiempo, abarcando el periodo 2007-2024.
+        </p>
       </div>
     </div>
   );
