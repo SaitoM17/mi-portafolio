96b0854fc33303d2d666c1a10fa7c6ef596cd42f
Said Mariano <smariano170@gmail.com>
2025-08-27 15:13:19 -0600

    feat: Se agrego apartado carga y exploración inicial

--- src/pages/PrecioTortillaMexico.js
+++ src/pages/PrecioTortillaMexico.js
@@ -1,4 +1,23 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
 function PrecioTortilla() {
+
+  const code1 = `# Identificar valores nulos
+print('Identificar Valores Nulos por Columnas')
+valores_nulos = df_tortilla_price.isnull().sum()
+print(valores_nulos)`;
+
+  const salida1 = `Identificar Valores Nulos por Columnas
+State                    0
+City                     0
+Year                     0
+Month                    0
+Day                      0
+Store type               0
+Price per kilogram    6390
+dtype: int64`;
+
   return (
     <div className="pagina-proyecto">
       <div className="contenedor-proyecto">
@@ -19,6 +38,21 @@ function PrecioTortilla() {
         </p>
 
         <h2>🧪 Desarrollo del Proyecto</h2>
+
+        <h3>Carga y exploración inicial de los datos</h3>
+        <p>
+          El proyecto comenzó con la obtención del conjunto de datos sobre los precios de la tortilla de maíz en México desde el año 2007 hasta el 2024, 
+          publicado en kaggle por Rick Chavelas. Se realizó una exploración preliminar para entender la estructura del dataset, la cantidad de registros, 
+          las variables disponibles, y la granularidad temporal y geográfica. Esta fase incluyó el uso de funciones como .head(), .info() y .describe() para 
+          detectar inconsistencias básicas y comprender las dimensiones generales del problema.
+        </p>
+        <p>El problema identificando fue la falta de datos en la columna Price per kilogram</p>
+        <SyntaxHighlighter language="python" style={dracula} className="code-block">
+          {code1}
+        </SyntaxHighlighter>
+        <SyntaxHighlighter language="bash" style={dracula} className="code-block">
+          {salida1}
+        </SyntaxHighlighter>
       </div>
     </div>
   );
