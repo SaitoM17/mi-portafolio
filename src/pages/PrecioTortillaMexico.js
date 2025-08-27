8048f4c72974cf53b42541cddea15978e1683212
Said Mariano <smariano170@gmail.com>
2025-08-27 14:10:24 -0600

    feat: Se agrego titulo e imagen del proyecto

--- src/pages/PrecioTortillaMexico.js
+++ src/pages/PrecioTortillaMexico.js
@@ -2,6 +2,8 @@ function PrecioTortilla() {
   return (
     <div className="pagina-proyecto">
       <div className="contenedor-proyecto">
+        <h1>📊 Precio de las tortillas en México</h1>
+        <img src="/img/Tortillas.png" alt="Amazon Delivary" className="imagen-principal" />
       </div>
     </div>
   );
