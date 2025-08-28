be78af1e3bc6147d4f54ff12a8841aac502bb1d6
Said Mariano <smariano170@gmail.com>
2025-08-28 13:44:07 -0600

    feat: Se agrego propósito

--- src/pages/MexicoTradeStati.js
+++ src/pages/MexicoTradeStati.js
@@ -5,6 +5,13 @@ function MexicoTradeStatis() {
 
         <h1>📊 Análisis "Mexico Trade Statistics 2012 - 2023"</h1>
         <img src="/img/Trade_Statistics.jpg" alt="Trade Principal" className="imagen-principal"/>
+
+        <h2>🎯 Propósito</h2>
+        <p>
+          Analizar la evolución del comercio exterior de México entre 2012 y 2023, identificando los principales 
+          socios comerciales, productos clave, tendencias de exportación e importación, y patrones económicos 
+          relevantes mediante técnicas de análisis de datos.
+        </p>
       </div>
     </div>
   );
