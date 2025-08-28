f6ef6092d6813845676c66a79684179b43c0f1bf
Said Mariano <smariano170@gmail.com>
2025-08-28 10:49:43 -0600

    feat: Titulo e imagen principal del proyecto

--- src/pages/SIEM.js
+++ src/pages/SIEM.js
@@ -2,6 +2,10 @@ function SIEM() {
   return (
     <div className="pagina-proyecto">
       <div className="contenedor-proyecto">
+
+        <h1>📊 Sistema de Información Empresarial Mexicano (SIEM)</h1>
+        <img src="/img/SIEM.jpg" alt="SIEM" className="imagen-principal"/>
+        <br/>
       </div>
     </div>
   );
