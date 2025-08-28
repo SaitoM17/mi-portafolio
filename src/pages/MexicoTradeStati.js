568874606d4d55e3ad442d96aa2cd175919ce3cc
Said Mariano <smariano170@gmail.com>
2025-08-28 13:42:09 -0600

    feat: Titulo principal e imagen

--- src/pages/MexicoTradeStati.js
+++ src/pages/MexicoTradeStati.js
@@ -2,6 +2,9 @@ function MexicoTradeStatis() {
   return (
     <div className="pagina-proyecto">
       <div className="contenedor-proyecto">
+
+        <h1>📊 Análisis "Mexico Trade Statistics 2012 - 2023"</h1>
+        <img src="/img/Trade_Statistics.jpg" alt="Trade Principal" className="imagen-principal"/>
       </div>
     </div>
   );
