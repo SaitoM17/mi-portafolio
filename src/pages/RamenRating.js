6825c699be0e4d2f9da5e2e103db30c4cc91b1a0
Said Mariano <smariano170@gmail.com>
2026-01-02 18:08:43 -0600

    refactor: Actualización de como se presentan las paginas

--- src/pages/RamenRating.js
+++ src/pages/RamenRating.js
@@ -1,52 +1,13 @@
+import React from 'react';
+import ReadmeViewer from '../components/ReadmeViewer';
 import "./proyectPages.css";
 
-function RamenRantings() {
+const RamenRantings = () => {
   return (
-    <div className="pagina-proyecto">
-      <div className="contenedor-proyecto">
-
-        <h1>🍵 Ramen Ratings</h1>
-        <img src="/img/Ramen.jpg" alt="Ramen" className="imagen-ramen" />
-        <br/>
-
-        <h2>Analisis de Ramen (Ramen Ratings Dashboard - Power BI)</h2>
-        <p>
-          Este proyecto presenta un dashboard interactivo basado en más de 2500 reseñas de productos de ramen a nivel internacional, recopiladas por Aleksey Bilogur.
-        </p>
-
-        <h2>🎯 Propósito</h2>
-        <p>Visualizar y analizar las calificaciones de ramen para identificar:</p>
-        <lu>
-          <li>Las marcas mejor calificadas</li>
-          <li>El estilo de empaque con mejores evaluaciones</li>
-          <li>Los países con mayores promedios de estrellas</li>
-          <li>Productos destacados de 2012 a 2016</li>
-        </lu>
-
-        <h2>📌 Vista previa del dashboard</h2>
-        <img src="/RamenRatings/Dashboard_ramen_ratings.png" alt="Ramen Rating " className="imagen-proyecto"/>
-
-        <h2>💡 Insight clave</h2>
-        <p>
-          Aunque Nissin es la marca con más productos evaluados (368), MyKuali es la mejor calificada con un promedio de ⭐ 4.95 estrellas entre 22 productos.
-        </p>
-
-        <h2>🧠 Lecciones aprendidas</h2>
-        <lu>
-          <li>Cómo aplicar filtros efectivos (por volumen de productos) para evitar sesgos.</li>
-          <li>Cómo destacar insights narrativos que expliquen el valor del análisis.</li>
-          <li>Uso de Power BI para presentar dashboards listos para ejecutivos.</li>
-        </lu>
-
-        <br/>
-
-        <a href="https://github.com/SaitoM17/ramen_ratings.git" target="_blank" rel="noopener noreferrer">
-          Ver en GitHub
-        </a>
-
-      </div>
-    </div>
+    <>
+      <ReadmeViewer repoName="ramen_ratings" /> 
+    </>
   );
-}
+};
 
 export default RamenRantings;
